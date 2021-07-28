3f26b8175fe1d45b032edcae153e53b6cf7aef7a
Ben Davis <ben.david.davis16@gmail.com>
2021-07-28 15:12:59 +0100

    CH13

--- lib/station.rb
+++ lib/station.rb
@@ -1,3 +1,15 @@
-class station
+class Station
+  attr_reader :name, :zone
+  def initialize(name, zone)
+    @name = name
+    @zone = zone
+  end
 
+  # def name
+  #   @name
+  # end
+
+  # def zone
+  #   @zone
+  # end
 end
